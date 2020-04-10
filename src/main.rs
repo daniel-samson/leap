a571bb7cb24dbade40dcbb6b26512f7014d0e75c
daniel-samson <daniel.samson@fastmail.com>
2020-04-10 11:50:26 +0100

    Getting version tags

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,13 @@ use serde_json::Value;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    println!("Getting tags_url");
+    println!("Versions:");
     let tags_url = get_github_tags_url().await?;
     let tags = get_github_tags(tags_url).await?;
-    for tag in tags.into_iter() {
-        let name = tag["name"].as_str().unwrap();
-        let download_url = tag["zipball_url"].as_str().unwrap();
-        println!("{}: {}", name, download_url);
+    let versions = get_versions_from_tags(tags);
+
+    for version in versions {
+        println!("{}: {}", version.name, version.url);
     }
 
     Ok(())
@@ -51,4 +51,25 @@ async fn get_github(url: &str) -> Result<Value, Box<dyn std::error::Error + Send
     let reader = body.reader();
     let v: Value = serde_json::from_reader(reader).unwrap();
     Ok(v)
+}
+
+fn get_versions_from_tags(tags: Vec<Value>) -> Vec<Version> {
+    tags.iter()
+        .map(|tag| {
+            (
+                tag["name"].as_str().unwrap(),
+                tag["zipball_url"].as_str().unwrap(),
+            )
+        })
+        .filter(|(name, _)| name.starts_with("v"))
+        .map(|(name, url)| Version {
+            name: String::from(name.trim_start_matches('v')),
+            url: String::from(url),
+        })
+        .collect()
+}
+
+struct Version {
+    name: String,
+    url: String,
 }
