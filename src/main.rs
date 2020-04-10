a7f5610febce26498474b3407d7493d58dcd8a63
daniel-samson <daniel.samson@fastmail.com>
2020-04-10 11:17:38 +0100

    Refactoring code to make it easier to read

--- src/main.rs
+++ src/main.rs
@@ -7,28 +7,29 @@ use serde_json::Value;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     println!("Getting tags_url");
-    let res = get_github("https://api.github.com/repos/daniel-samson/leap").await?;
-    let tags = match res["tags_url"].as_str() {
-        None => panic!("failed to get tags_url"),
-        Some(tags_url) => {
-            println!("Getting tags");
-            let res = get_github(tags_url).await?;
-            let tags = res.as_array().cloned();
-            tags
-        }
-    };
-    match tags {
-        None => panic!("failed to get tags"),
-        Some(tags) => {
-            for tag in tags.into_iter() {
-                let name = tag["name"].as_str().unwrap();
-                let download_url = tag["zipball_url"].as_str().unwrap();
-                println!("{}: {}", name, download_url);
-            }
-        }
+    let tags_url = get_github_tags_url().await?;
+    let tags = get_github_tags(tags_url).await?;
+    for tag in tags.into_iter() {
+        let name = tag["name"].as_str().unwrap();
+        let download_url = tag["zipball_url"].as_str().unwrap();
+        println!("{}: {}", name, download_url);
     }
 
     Ok(())
+}
+
+async fn get_github_tags_url() -> Result<String, Box<dyn std::error::Error + Send + Sync>> {
+    let res = get_github("https://api.github.com/repos/daniel-samson/leap").await?;
+    let tags_url = res["tags_url"].as_str().expect("failed to get tags_url");
+    Ok(String::from(tags_url))
+}
+
+async fn get_github_tags(
+    tags_url: String,
+) -> Result<Vec<Value>, Box<dyn std::error::Error + Send + Sync>> {
+    let res = get_github(tags_url.as_str()).await?;
+    let tags = res.as_array().expect("failed to parse tags");
+    Ok(tags.clone())
 }
 
 async fn get_github(url: &str) -> Result<Value, Box<dyn std::error::Error + Send + Sync>> {
